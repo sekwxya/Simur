3857ed2bc6a470fc41f4252d7f5c253091f51b8d
GrishaPC <GrishaPC@DESKTOP-7K1NT4K>
2024-12-06 21:59:00 +0300

    Добавьте файлы проекта.

---
+++ TourAgency/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace TourAgency.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "Visitor" или "TourManager"
+        public int LoyaltyPoints { get; set; } // Бонусные баллы
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<TourRequest> TourRequests { get; set; }
+    }
+}
