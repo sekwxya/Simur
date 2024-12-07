5f777bf0453c938ce511059c98d116272448af9a
svinkizy <RAZDWA77@yandex.ru>
2024-12-07 18:23:45 +0300

    Add db

---
+++ TourAgency/Controllers/ToursController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TourAgency.Data;
+using TourAgency.Models;
+
+namespace TourAgency.Controllers
+{
+    public class ToursController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ToursController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tours
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tour.ToListAsync());
+        }
+
+        // GET: Tours/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tour = await _context.Tour
+                .FirstOrDefaultAsync(m => m.TourId == id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            return View(tour);
+        }
+
+        // GET: Tours/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tours/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TourId,Title,Description,Country,Price,StartDate,EndDate,IsHot")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tour);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tour = await _context.Tour.FindAsync(id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Tours/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TourId,Title,Description,Country,Price,StartDate,EndDate,IsHot")] Tour tour)
+        {
+            if (id != tour.TourId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tour);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TourExists(tour.TourId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tour = await _context.Tour
+                .FirstOrDefaultAsync(m => m.TourId == id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            return View(tour);
+        }
+
+        // POST: Tours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tour = await _context.Tour.FindAsync(id);
+            if (tour != null)
+            {
+                _context.Tour.Remove(tour);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TourExists(int id)
+        {
+            return _context.Tour.Any(e => e.TourId == id);
+        }
+    }
+}
--- TourAgency/Program.cs
+++ TourAgency/Program.cs
@@ -33,7 +33,7 @@ namespace TourAgency
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Tours}/{action=Index}/{id?}");
 
             app.Run();
         }
