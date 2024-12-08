df497b5f16a201050411dfb6b12521d770513470
svinkizy <RAZDWA77@yandex.ru>
2024-12-08 22:36:55 +0300

    last version

--- TourAgency/Controllers/DiscountsController.cs
+++ TourAgency/Controllers/DiscountsController.cs
@@ -10,29 +10,22 @@ using TourAgency.Models;
 
 namespace TourAgency.Controllers
 {
-    public class ToursController : Controller
+    public class DiscountsController : Controller
     {
         private readonly AppDbContext _context;
 
-        public ToursController(AppDbContext context)
+        public DiscountsController(AppDbContext context)
         {
             _context = context;
         }
 
-        // GET: Tours
+        // GET: Discounts
         public async Task<IActionResult> Index()
         {
-            var appDbContext = _context.Tour.Include(t => t.Discount);
-            return View(await appDbContext.ToListAsync());
+            return View(await _context.Discount.ToListAsync());
         }
 
-        //public IActionResult Discount()
-        //{
-        //    ViewData["DiscountId"] = new SelectList(_context.Discount, "DiscountId", "Name");
-        //    return View();
-        //}
-
-        // GET: Tours/Details/5
+        // GET: Discounts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -40,41 +33,39 @@ namespace TourAgency.Controllers
                 return NotFound();
             }
 
-            var tour = await _context.Tour
-                .Include(t => t.Discount)
-                .FirstOrDefaultAsync(m => m.TourId == id);
-            if (tour == null)
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.DiscountId == id);
+            if (discount == null)
             {
                 return NotFound();
             }
 
-            return View(tour);
+            return View(discount);
         }
 
-        // GET: Tours/Create
+        // GET: Discounts/Create
         public IActionResult Create()
         {
-            ViewData["DiscountId"] = new SelectList(_context.Discount, "DiscountId", "Name");
             return View();
         }
 
-        // POST: Tours/Create
+        // POST: Discounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TourId,Title,Description,Country,Price,StartDate,EndDate,DiscountId")] Tour tour)
+        public async Task<IActionResult> Create([Bind("DiscountId,Name,DiscountPercentage")] Discount discount)
         {
-            tour.Discount = _context.Discount.Where(x => x.DiscountId == tour.DiscountId).FirstOrDefault();
             if (ModelState.IsValid)
             {
-                _context.Add(tour);
+                _context.Add(discount);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["DiscountId"] = new SelectList(_context.Discount, "DiscountId", "Name", tour.DiscountId);
-            return View(tour);
+            return View(discount);
         }
 
-        // GET: Tours/Edit/5
+        // GET: Discounts/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -82,21 +73,22 @@ namespace TourAgency.Controllers
                 return NotFound();
             }
 
-            var tour = await _context.Tour.FindAsync(id);
-            if (tour == null)
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount == null)
             {
                 return NotFound();
             }
-            ViewData["DiscountId"] = new SelectList(_context.Discount, "DiscountId", "DiscountId", tour.DiscountId);
-            return View(tour);
+            return View(discount);
         }
 
-        // POST: Tours/Edit/5
+        // POST: Discounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TourId,Title,Description,Country,Price,StartDate,EndDate,DiscountId")] Tour tour)
+        public async Task<IActionResult> Edit(int id, [Bind("DiscountId,Name,DiscountPercentage")] Discount discount)
         {
-            if (id != tour.TourId)
+            if (id != discount.DiscountId)
             {
                 return NotFound();
             }
@@ -105,12 +97,12 @@ namespace TourAgency.Controllers
             {
                 try
                 {
-                    _context.Update(tour);
+                    _context.Update(discount);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!TourExists(tour.TourId))
+                    if (!DiscountExists(discount.DiscountId))
                     {
                         return NotFound();
                     }
@@ -121,11 +113,10 @@ namespace TourAgency.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["DiscountId"] = new SelectList(_context.Discount, "DiscountId", "DiscountId", tour.DiscountId);
-            return View(tour);
+            return View(discount);
         }
 
-        // GET: Tours/Delete/5
+        // GET: Discounts/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,35 +124,34 @@ namespace TourAgency.Controllers
                 return NotFound();
             }
 
-            var tour = await _context.Tour
-                .Include(t => t.Discount)
-                .FirstOrDefaultAsync(m => m.TourId == id);
-            if (tour == null)
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.DiscountId == id);
+            if (discount == null)
             {
                 return NotFound();
             }
 
-            return View(tour);
+            return View(discount);
         }
 
-        // POST: Tours/Delete/5
+        // POST: Discounts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var tour = await _context.Tour.FindAsync(id);
-            if (tour != null)
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount != null)
             {
-                _context.Tour.Remove(tour);
+                _context.Discount.Remove(discount);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool TourExists(int id)
+        private bool DiscountExists(int id)
         {
-            return _context.Tour.Any(e => e.TourId == id);
+            return _context.Discount.Any(e => e.DiscountId == id);
         }
     }
 }
--- TourAgency/Models/Tour.cs
+++ TourAgency/Models/Tour.cs
@@ -15,5 +15,29 @@ namespace TourAgency.Models
         public Discount? Discount { get; set; }
         public List<TourPlan> tourPlans { get; set; } = new();
         public List<Review> reviews { get; set; } = new();
+
+        // Метод для проверки, является ли тур "горящим"
+        public bool IsHotTour()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            var daysUntilStart = StartDate.DayNumber - today.DayNumber;
+            return daysUntilStart <= 7; // Тур считается "горящим", если до начала осталось 7 дней или меньше
+        }
+
+        // Метод для расчета цены с учетом скидки
+        public decimal GetDiscountedPrice(Discount hotTourDiscount)
+        {
+            var basePrice = Price;
+            if (Discount != null)
+            {
+                basePrice -= basePrice * Discount.DiscountPercentage;
+            }
+            if (IsHotTour() && DiscountId == 1) // Применяем скидку на "горящие туры" только если DiscountId = 1
+            {
+                basePrice -= basePrice * hotTourDiscount.DiscountPercentage;
+            }
+            return basePrice;
+        }
     }
+
 }
