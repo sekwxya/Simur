aa680368979de3616cb5be6673eeb8892abb6ced
svinkizy <RAZDWA77@yandex.ru>
2024-12-17 00:13:53 +0300

    managerpanel

--- TourAgency/Controllers/TourRequestController.cs
+++ TourAgency/Controllers/TourRequestController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TourAgency.Data;
 using TourAgency.Models;
@@ -19,14 +14,79 @@ namespace TourAgency.Controllers
             _context = context;
         }
 
+        // Список всех заявок
         public async Task<IActionResult> Index()
         {
-            return View(await _context.TourRequest.ToListAsync());
+            var tourRequests = await _context.TourRequest
+                .Include(tr => tr.User)
+                .Where(tr => tr.Status == "На рассмотрении") // Фильтруем только заявки "На рассмотрении"
+                .ToListAsync();
+
+            return View(tourRequests);
         }
 
-        public IActionResult Create()
+        // Отображение деталей заявки
+        public async Task<IActionResult> Details(int id)
         {
-            return View();
+            var tourRequest = await _context.TourRequest
+                .Include(tr => tr.User)
+                .FirstOrDefaultAsync(tr => tr.TourRequestId == id);
+
+            if (tourRequest == null)
+            {
+                return NotFound();
+            }
+
+            var tours = await _context.Tour.ToListAsync();
+            ViewBag.Tours = tours;
+
+            return View(tourRequest);
+        }
+
+        // Подтверждение заявки
+        [HttpPost]
+        public async Task<IActionResult> Approve(int id, int TourId)
+        {
+            var tourRequest = await _context.TourRequest.FindAsync(id);
+
+            if (tourRequest == null)
+            {
+                return NotFound();
+            }
+
+            var tour = await _context.Tour.FindAsync(TourId);
+            if (tour == null)
+            {
+                return BadRequest("Выбранный тур не найден.");
+            }
+
+            // Обновление статуса заявки
+            tourRequest.Status = "Одобрено";
+
+            // Сохранение изменений
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Отклонение заявки
+        [HttpPost]
+        public async Task<IActionResult> Reject(int id)
+        {
+            var tourRequest = await _context.TourRequest.FindAsync(id);
+
+            if (tourRequest == null)
+            {
+                return NotFound();
+            }
+
+            // Обновление статуса заявки
+            tourRequest.Status = "Отклонено";
+
+            // Сохранение изменений
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
