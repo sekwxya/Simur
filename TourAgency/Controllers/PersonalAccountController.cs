a68c1723e74f8b09f9c23529cce9c3933867bbfa
sekwxyaRe <tima.latipov.09@gmail.com>
2024-12-16 20:15:54 +0300

    Create controller, view, button personal account

---
+++ TourAgency/Controllers/PersonalAccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TourAgency.Data;
+using TourAgency.Models;
+
+namespace TourAgency.Controllers
+{
+    public class PersonalAccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PersonalAccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var user = new User
+            {
+                FullName = "Имя Пользователя",
+                Balance = 12500.75m,
+                LoyaltyPoints = 320
+            };
+            return View(user);
+        }
+    }
+}
